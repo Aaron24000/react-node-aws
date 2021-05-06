23774171cc10c6c856fe0bee8e8466b6b1c5c925
Aaron24000 <aaron.nobles@outlook.com>
2021-05-06 02:45:04 -0400

    Added basic API portion for registration

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+// Import validators
+const {userRegisterValidator} = require('../validators/auth');
+const {runValidation} = require('../validators');
+
 // import from controllers
 const { register } = require('../controllers/auth');
 
-router.get('/register', register);
+router.post('/register', userRegisterValidator, runValidation, register);
 
 module.exports = router;
