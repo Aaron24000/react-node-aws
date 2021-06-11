b417a0dd2e8653cfaf0ab4e2285f9c48654e5d50
Aaron24000 <aaron.nobles@outlook.com>
2021-06-11 11:10:14 -0400

    fixed link slugs too

--- server/routes/link.js
+++ server/routes/link.js
@@ -12,9 +12,9 @@ const { create, list, read, update, remove } = require('../controllers/link');
 // routes
 router.post('/link', linkCreateValidator, runValidation, requireSignin, authMiddleware, create);
 router.get('/links', list);
-router.get('/link:slug', read);
-router.put('/link:slug', linkUpdateValidator, runValidation, requireSignin, authMiddleware, update);
-router.delete('/link:slug', requireSignin, authMiddleware, remove);
+router.get('/link/:slug', read);
+router.put('/link/:slug', linkUpdateValidator, runValidation, requireSignin, authMiddleware, update);
+router.delete('/link/:slug', requireSignin, authMiddleware, remove);
 
 module.exports = router;
 
